2e92093f184dfd5aa87dfd69a06387346682c2b3
ochampo <danocampo@meta.com>
2025-12-31 00:16:57 -0800

    Testing the preview robot name

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => {
-  // If we are building for a PR Preview, we need a relative base path
-  // so it works in the /pr-preview/pr-X/ subfolder
+  // Check if this is a GitHub Actions environment
   const isPreview = process.env.GITHUB_EVENT_NAME === 'pull_request';
-
+  
   return {
     plugins: [react()],
+    // If it's a preview, use relative paths ('./') 
+    // If it's the live site, use the root ('/')
     base: isPreview ? './' : '/', 
   }
 })
