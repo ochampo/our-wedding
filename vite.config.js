ad66c549a6216780d97352012d0b057a03a6097a
ochampo <danocampo@meta.com>
2026-01-01 02:45:36 -0800

    changing text and testing out pull request prev

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  // 1. Detect if we are in a GitHub Action Pull Request
-  // GitHub sets GITHUB_EVENT_NAME to 'pull_request' automatically
+  // Check if this is a PR build inside GitHub Actions
   const isPreview = process.env.GITHUB_EVENT_NAME === 'pull_request';
-  
-  // 2. Get the PR number from the Ref (e.g., 'refs/pull/1/merge' -> '1')
   const prNumber = process.env.GITHUB_REF ? process.env.GITHUB_REF.split('/')[2] : '';
 
   return {
     plugins: [react()],
-    // 3. Set thse base path
-    // If it's a PR: /pr-preview/pr-1/
-    // If it's live: /
+    // This is the magic line:
+    // It makes your app work at 'lorraineanddaniel.com/pr-preview/pr-X/'
     base: isPreview ? `/pr-preview/pr-${prNumber}/` : '/',
+    build: {
+      outDir: 'dist',
+    }
   }
 })
