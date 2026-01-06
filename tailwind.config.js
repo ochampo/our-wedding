50231d5c353d890dbfead62e7166e31e7666971b
Daniel Ocampo <ochampo77@gmail.com>
2026-01-06 13:50:52 -0800

    fix: Tailwind styles not loading due to duplicate config files- Fixed an issue where 	ailwind.config.js.js was being edited instead of the active config file.- Updated the correct 	ailwind.config.js content paths to ensure styles are generated for src and root files.- Cleaned up redundant configuration files. (#5)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // This line is vital!
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}", // <--- Vital for root files
   ],
   theme: {
     extend: {},
