3061cc760f953f738988eb5fee1ce2138b8ee726
ochampo <danocampo@meta.com>
2026-01-06 23:22:13 -0800

    removing console for debugging, also added the variable into github

--- src/App.jsx
+++ src/App.jsx
@@ -72,8 +72,6 @@ const WeddingSite = () => {
 
 // NEW (Works in Vite)
   const GOOGLE_URL = import.meta.env.VITE_GOOGLE_SCRIPT_URL;
-  console.log("My URL is:", GOOGLE_URL);
-  console.log("Environment Variables:")
   // --- REUSABLE DATA ---
   const LOCATIONS = [
     {
